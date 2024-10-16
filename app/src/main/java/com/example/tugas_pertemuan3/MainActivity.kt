70fa1694f1f7cf3fe0c69d69aceeda713d3023a8
defarrel <Defarrel>
2024-10-16 19:36:14 +0700

    Memberikan style pada text versus

--- app/src/main/java/com/example/tugas_pertemuan3/MainActivity.kt
+++ app/src/main/java/com/example/tugas_pertemuan3/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
@@ -48,7 +49,10 @@ fun BasicCompose(dfrl: Modifier = Modifier){
             color = Color.Red,
             fontWeight = FontWeight.Bold
         )
-        Text(text = "Versus")
+        Text(text = "Versus",
+            fontSize = 50.sp,
+            color = Color.DarkGray,
+            fontStyle = FontStyle.Italic)
     }
 }
 
