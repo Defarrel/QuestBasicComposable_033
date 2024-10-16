3370fc3b396acfffacafbdaa9650ad39a7f3752a
defarrel <Defarrel>
2024-10-16 20:30:48 +0700

    Menambahkan dan menampilkan foto saya dan mengubahnya menjadi bentuk lingkaran

--- app/src/main/java/com/example/tugas_pertemuan3/MainActivity.kt
+++ app/src/main/java/com/example/tugas_pertemuan3/MainActivity.kt
@@ -10,12 +10,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -75,6 +78,14 @@ fun BasicCompose(modifier: Modifier = Modifier){
             fontSize = 50.sp,
             color = Color.Black,
             fontWeight = FontWeight.Bold
+        )
+        Image(
+            painter = painterResource(id = R.drawable.me),
+            contentDescription = null,
+            modifier = Modifier
+                .size(200.dp)
+                .clip(CircleShape),
+            contentScale = ContentScale.Crop
         )
     }
 }
