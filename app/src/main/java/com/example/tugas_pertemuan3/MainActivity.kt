839175f6b55de0b09306a9eca909bf702daa9b95
defarrel <Defarrel>
2024-10-16 19:33:57 +0700

    Menambahkan column berisi verticalArrangement dan horizontalAlignment

--- app/src/main/java/com/example/tugas_pertemuan3/MainActivity.kt
+++ app/src/main/java/com/example/tugas_pertemuan3/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.tugas_pertemuan3.ui.theme.Tugas_Pertemuan3Theme
@@ -33,8 +35,14 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun BasicCompose(dfrl: Modifier = Modifier){
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ){
         Text(text = "Indonesia", modifier = dfrl)
         Text(text = "Versus")
+    }
 }
 
 @Composable
