98910797c86afb484b21dcf585192dbcbb9e6550
defarrel <Defarrel>
2024-10-16 19:35:21 +0700

    Memberikan style pada text indonesia

--- app/src/main/java/com/example/tugas_pertemuan3/MainActivity.kt
+++ app/src/main/java/com/example/tugas_pertemuan3/MainActivity.kt
@@ -13,7 +13,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.tugas_pertemuan3.ui.theme.Tugas_Pertemuan3Theme
 
 class MainActivity : ComponentActivity() {
@@ -40,7 +43,11 @@ fun BasicCompose(dfrl: Modifier = Modifier){
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
     ){
-        Text(text = "Indonesia", modifier = dfrl)
+        Text(text = "Indonesia", modifier = dfrl,
+            fontSize = 70.sp,
+            color = Color.Red,
+            fontWeight = FontWeight.Bold
+        )
         Text(text = "Versus")
     }
 }
