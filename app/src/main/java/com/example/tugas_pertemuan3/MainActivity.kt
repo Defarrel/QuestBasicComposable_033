5d517fc890faa1dea4cc087bbe39433bf2dd4a02
defarrel <Defarrel>
2024-10-16 19:40:12 +0700

    Menampilkan image dan memberi image batasan ukuran

--- app/src/main/java/com/example/tugas_pertemuan3/MainActivity.kt
+++ app/src/main/java/com/example/tugas_pertemuan3/MainActivity.kt
@@ -4,19 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.tugas_pertemuan3.ui.theme.Tugas_Pertemuan3Theme
 
@@ -53,6 +57,10 @@ fun BasicCompose(dfrl: Modifier = Modifier){
             fontSize = 50.sp,
             color = Color.DarkGray,
             fontStyle = FontStyle.Italic)
+        Image(painter = painterResource(id = R.drawable.afc),
+            contentDescription = null,
+            modifier = Modifier.size(200.dp)
+        )
     }
 }
 
